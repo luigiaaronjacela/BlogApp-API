5c7c0e86e7cd9a1fd7eda799a80726dc3e440009
luigiaaronjacela <luigiaaronjacela@gmail.com>
2025-07-18 20:29:21 +0800

    update server link

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 
 // CORS Config
 const corsOptions = {
-  origin: ['http://localhost:3000', 'http://localhost:4000'],
+  origin: ['http://localhost:3000', 'http://localhost:4000', 'https://your-api.onrender.com/blogs'],
   credentials: true,
   optionsSuccessStatus: 200,
 };
